f9baaa34dbdce743ef6475428534f4d341975ff1
tkdals1144 <146908349+tkdals1144@users.noreply.github.com>
2025-12-29 23:18:11 +0900

    update

--- my-react-app/src/features/banner/components/Banner.tsx
+++ my-react-app/src/features/banner/components/Banner.tsx
@@ -3,15 +3,15 @@ import { increase, decrease } from "../bannerSlice";
 import type { RootState } from "./../../../app/store";
 import { useState } from "react";
 import styles from "./Banner.module.css";
-import List from "./list";
+import List from "./List";
 
 const Banner = () => {
     const [view, setView] = useState<"main" | "list" | "Img">("main");
     const dispatch = useDispatch();
     const score = useSelector((state: RootState) => state.score.score);
-    return (
-        <>
-            {view === "main" && (
+    switch (view) {
+        case "main":
+            return (
                 <>
                     <h1>{score}</h1>
                     <button onClick={() => dispatch(increase())}>상승</button>
@@ -21,14 +21,10 @@ const Banner = () => {
                         <button onClick={() => setView("Img")}>이미지 이동</button>
                     </div>
                 </>
-            )}
-            {view === "list" && (
-                <>
-                    <List />
-                </>
-            )}
-        </>
-    );
+            );
+        case "list":
+            return <List />;
+    }
 };
 
 export default Banner;
